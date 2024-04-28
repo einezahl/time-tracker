a33a45a3ed8bd6d7ba743f0c9c89008fd503ed34
einezahl <tom.reclik@gmail.com>
2024-04-28 19:04:54 +0200

    Add basic functionality to add a work time element to a document under a specific keypoint

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Menu, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -6,79 +6,52 @@ interface MyPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface WorkTimeElement{
+	start_time: string;
+	end_time: string;
+	details: string;
+	category: string;
+
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HelloWorldPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
+		console.log("The plugin has been loaded.")
+		this.addRibbonIcon('dice', 'Open Menu', (event) => {
+			const menu = new Menu();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			menu.addItem((item) => 
+				item
+					.setTitle("Copy")
+					.setIcon("documents")
+					.onClick(() => {
+						new Notice("Copied");
+				})
+			)
+			menu.addItem((item) => 
+				item
+					.setTitle("Paste")
+					.setIcon("paste")
+					.onClick(() => {
+						new Notice("Pasted");
+				})
+			)
+			menu.showAtMouseEvent(event);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "add-time-element",
+			name: "Add work time element",
 			callback: () => {
-				new SampleModal(this.app).open();
+				new SampleModal(this.app, (workTimeElement) => {
+					this.openWorktimeNoteAndInsertData(workTimeElement);
+				}).open();
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		})
 	}
 
 	onunload() {
+		console.log("The plugin has been unloaded.")
 
 	}
 
@@ -89,16 +62,100 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async openWorktimeNoteAndInsertData(workTimeElement: WorkTimeElement) {
+		const file = await this.findOrCreateFile("worktime.md");
+		const leaf = this.app.workspace.getLeaf(true);
+		await leaf.openFile(file);
+
+		// Wait for the file to be fully loaded in the editor
+		setTimeout(() => this.insertDataInWorktimeFile(workTimeElement, file), 500);
+	}
+
+	async findOrCreateFile(filename: string) {
+		let file = this.app.vault.getAbstractFileByPath(filename);
+		if (!file) {
+			file = await this.app.vault.create(filename, "# Time Sheet\n"); // Create file with initial content if it does not exist
+		}
+		return file;
+	}
+
+	insertDataInWorktimeFile(workTimeElement: WorkTimeElement, file: TFile) {
+		const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+		if (editor) {
+			const doc = editor.getDoc();
+			const workTimeString = `${workTimeElement.start_time} - ${workTimeElement.end_time}: ${workTimeElement.details} - ${workTimeElement.category}\n`;
+
+			let found = false;
+			const lineNumber = doc.lastLine();
+			for (let i = 0; i <= lineNumber; i++) {
+				if (doc.getLine(i).startsWith('# Time Sheet')) {
+					doc.replaceRange(workTimeString, { line: i + 1, ch: 0 });
+					found = true;
+					break;
+				}
+			}
+
+			if (!found) { // If '# Time Sheet' heading is not found
+				doc.setValue(doc.getValue() + '\n# Time Sheet\n' + workTimeString);
+			}
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+export class SampleModal extends Modal {
+	workTimeElement: WorkTimeElement = {start_time: '', end_time: '', details: '', category: ''};
+	onSubmit: (workTimeElement: WorkTimeElement) => void;
+
+	constructor(app: App, onSubmit: (workTimeElement: WorkTimeElement) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.createEl("h1", {
+			text: "Enter work time details"
+		})
+		new Setting(contentEl)
+			.setName("Start Time")
+			.addText((text) => text.onChange(
+				(value) => {
+					this.workTimeElement.start_time = value
+		}) );
+		new Setting(contentEl)
+			.setName("End Time")
+			.addText((text) => text.onChange(
+				(value) => {
+					this.workTimeElement.end_time = value
+		}) );
+		new Setting(contentEl)
+			.addText((text) => text.onChange(
+				.setName("Details")
+				(value) => {
+					this.workTimeElement.details = value
+		}) );
+		new Setting(contentEl)
+			.setName("Category")
+			.addText((text) => text.onChange(
+				(value) => {
+					this.workTimeElement.category = value
+		}) );
+		new Setting(contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						console.log("test");
+						console.log(this.workTimeElement);
+						this.close();
+						this.onSubmit(
+							this.workTimeElement
+					);
+					}
+				)
+		)
 	}
 
 	onClose() {
